d62bd5d3e591925c66102951dbfad7ba05c1b4a4
danilkaiskhakoff-oss <danilka.iskhakoff@gmail.com>
2026-05-28 19:38:52 +0300

    fix: use namespace import for otpauth

--- client/src/components/simulations/TwoFactorAuth.jsx
+++ client/src/components/simulations/TwoFactorAuth.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
-import OTPAuth from 'otpauth';
+import * as OTPAuth from 'otpauth';
 import QRCode from 'qrcode.react';
 
 function TwoFactorAuth({ onComplete }) {
